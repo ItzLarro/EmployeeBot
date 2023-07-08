0f14ab4d1554b0b46b585e64c59ed5c2d42bd961
jef <fauthtech@gmail.com>
2023-07-07 20:00:05 -0400

    First Commit

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS TradeAds (
+        have TEXT,
+        want TEXT,
+        tradelink TEXT,
+        channel_id INTEGER,
+        user_id INTEGER,
+        next_post TEXT DEFAULT (datetime(CURRENT_TIMESTAMP, '+3 hours')),
+        PRIMARY KEY(channel_id, user_id)
+    );
+
+CREATE TABLE IF NOT EXISTS Giveaways (
+        prize TEXT,
+        end_time TEXT,
+        completed BOOLEAN DEFAULT FALSE,
+        channel_id INTEGER,
+        message_id INTEGER,
+        PRIMARY KEY(channel_id, message_id)
+    );
